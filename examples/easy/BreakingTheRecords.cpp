ef71d3064fc16a422521600c571004b5dd475b41
AlexeyChashchegorov <chashchegorov@gmail.com>
2018-08-01 13:13:08 +0300

    BreakingTheRecords example

---
+++ examples/easy/BreakingTheRecords.cpp
@@ -0,0 +1,37 @@
+/* 
+	Statements: https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem
+*/
+
+#include <common.h>
+
+vector<int> breakingRecords(vector<int> scores)
+{
+    if (scores.empty())
+        return {0,0};
+    auto min_score = scores[0];
+    auto max_score = scores[0];
+    vector<int> ret(2,0);
+    for (int i=1; i<scores.size(); i++)
+    {
+        ret[1] += (min_score > scores[i]) ? 1 : 0;
+        min_score = min(min_score, scores[i]);
+        ret[0] += (max_score < scores[i]) ? 1 : 0;
+        max_score = max(max_score, scores[i]);        
+    }
+    return ret;
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector<int> scores(n,0);
+	for(int i=0; i<n; i++)
+		cin >> scores[i];
+
+	vector<int> result = breakingRecords(scores);
+    	for (auto r: result)
+        	cout << r << " ";
+	cout << endl;
+	return 0;
+}

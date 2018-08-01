3c6f74fc89e6a5f87acc47f1f7941a7c2f4b806a
AlexeyChashchegorov <chashchegorov@gmail.com>
2018-08-01 12:15:07 +0300

    AppleAndOrange example, first view

---
+++ examples/easy/AppleAndOrange.cpp
@@ -0,0 +1,124 @@
+/*
+	Statements:https://www.hackerrank.com/challenges/apple-and-orange/problem
+*/
+
+
+#include <iostream>
+#include <list>
+#include <vector>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+list<int> absPoints(const vector<int>& v, int p)
+{
+    list<int> ret;
+    for(const auto& vv: v)
+        ret.push_back(p+vv);
+    return ret;
+}
+
+int countInRange(const list<int>& v, const pair<int,int>& from_to)
+{
+    return count_if(v.begin(), v.end(), 
+        [&from_to](int vv){return (vv >= from_to.first) && (vv <= from_to.second);});   
+}
+
+// Complete the countApplesAndOranges function below.
+void countApplesAndOranges(int s, int t, int a, int b, const vector<int>& apples, const vector<int>& oranges) 
+{
+    auto from_to = make_pair(s,t);
+    cout << countInRange(absPoints(apples, a), from_to) << endl;
+    cout << countInRange(absPoints(oranges, b), from_to) << endl;
+}
+
+int main()
+{
+    string st_temp;
+    getline(cin, st_temp);
+
+    vector<string> st = split_string(st_temp);
+
+    int s = stoi(st[0]);
+
+    int t = stoi(st[1]);
+
+    string ab_temp;
+    getline(cin, ab_temp);
+
+    vector<string> ab = split_string(ab_temp);
+
+    int a = stoi(ab[0]);
+
+    int b = stoi(ab[1]);
+
+    string mn_temp;
+    getline(cin, mn_temp);
+
+    vector<string> mn = split_string(mn_temp);
+
+    int m = stoi(mn[0]);
+
+    int n = stoi(mn[1]);
+
+    string apples_temp_temp;
+    getline(cin, apples_temp_temp);
+
+    vector<string> apples_temp = split_string(apples_temp_temp);
+
+    vector<int> apples(m);
+
+    for (int i = 0; i < m; i++) {
+        int apples_item = stoi(apples_temp[i]);
+
+        apples[i] = apples_item;
+    }
+
+    string oranges_temp_temp;
+    getline(cin, oranges_temp_temp);
+
+    vector<string> oranges_temp = split_string(oranges_temp_temp);
+
+    vector<int> oranges(n);
+
+    for (int i = 0; i < n; i++) {
+        int oranges_item = stoi(oranges_temp[i]);
+
+        oranges[i] = oranges_item;
+    }
+
+    countApplesAndOranges(s, t, a, b, apples, oranges);
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
+
